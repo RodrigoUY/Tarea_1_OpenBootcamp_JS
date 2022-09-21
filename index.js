ce5bf87669276982d5d75c85e791a1dce8bc7820
RodrigoUY <rodree.11@gmail.com>
2022-09-21 14:54:49 -0300

    Se agrega la tarea

---
+++ index.js
@@ -0,0 +1,12 @@
+// Este es el mensaje de bienvenida
+console.log("Hola, este es mi primer ejercicio con Node en el mejor Bootcamp de programación del mundo");
+
+ /*
+    
+    Esta es la parte intermedia de nuestro proyecto
+    En esta parte realizamos los chequeos necesarios
+    Por ultimo vamos a la ultima linea de saludo
+ */
+
+// Este va a ser el mensaje que se envia para despedir
+console.log("Adiós")
